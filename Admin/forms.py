fbf34a7fd65320d1d0d71395813f89012e840f26
AnvarNarzullayev <anvarnarzullayev12@gmail.com>
2022-03-05 10:23:21 +0500

    postcreateform yaratildi

--- Admin/forms.py
+++ Admin/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Category
+from .models import Category, Post
 
 class CreateCategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class CreatePostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['name','slug','img','short_info','description','category','tags','status','is_main']
